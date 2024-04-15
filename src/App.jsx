51cfe6bdf2d644b67377d57de64a45041f540378
DESKTOP-B9UVUDU\Bibi <beniamin.kumala@gmail.com>
2024-04-15 10:05:37 +0200

    Menu mini project in react first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu"></Title>
+        <Categories
+          filterItems={filterItems}
+          categories={categories}
+        ></Categories>
+        <Menu items={menuItems}></Menu>
+      </section>
+    </main>
+  );
+};
+export default App;
